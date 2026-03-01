aff49e32b8786cae674f0796f3976dda96627205
Subashini2106 <s3977481@student.rmit.edu.au>
2026-03-02 00:57:43 +1100

    update url server.js

--- Movie_Review_App/my-app/backend/server.js
+++ Movie_Review_App/my-app/backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://my-movie-log.netlify.app/'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://my-movie-log.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
